d69b66563539654588a3714f34b3631cf8ece67c
kfruiz2007 <kfruiz2007@gmail.com>
2026-07-27 22:01:44 -0600

    Login validation

---
+++ js/pages/login.js
@@ -0,0 +1,40 @@
+document.getElementById("loginForm").addEventListener("submit", async function(e) {
+        e.preventDefault(); // Prevent form submission
+
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+        const messageElement = document.getElementById("message");
+
+        try {
+           const response = await fetch("assets/data/user.json");
+            
+            if (!response.ok) {
+                throw new Error("This user does not exist.");
+            }
+
+            const users = await response.json();
+            const userFound = users.find(u => u.username === username && u.password === password);
+
+            if (userFound) {
+                messageElement.textContent = "Login successful!";
+                messageElement.style.color = "green";
+                // Redirect to another page or perform other actions
+            localStorage.setItem("user", JSON.stringify(userFound));
+
+            setTimeout(() => {
+                window.location.href = "index.html"; // Redirect to index.html after 2 seconds
+            }, 2000);
+        }
+    
+
+        else {
+            messageElement.textContent = "Invalid username or password.";
+            messageElement.style.color = "red";
+        }
+    }
+        catch (error) {
+            messageElement.textContent = error.message;
+            messageElement.style.color = "red";
+        }
+    
+});
